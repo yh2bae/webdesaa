ba84d8f82ab9a5c1aadb6fa6ca876450bbe9934b
IchwanSetyawan <ichwansetyawan2015@gmail.com>
2021-09-21 16:21:29 +0700

    update style slider carraousel and home frontend

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -3,64 +3,132 @@
 @section('content')
     <article class=" p-4 shadow-sm my-3 bg-body rounded">
         <div class="row">
-            <div class="col-sm-6 col-md-4">
-                <figure>
-                    <img src="{{ asset('frontend/assets/sinergi2.jpg') }}">
-                </figure>
-            </div>
-            <div class="col-sm-6 col-md-8">
-                <h4>Raymond Dragon</h4>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-                    laoreet dolore magna aliquam erat volutpat.</p>
-
-                <section>
-                    <i class="icon-user"></i>Ichwan Setyawan
-                    <i class="icon-calendar"></i>1395/12/21
-                    <a href="#" class="btn btn-primary btn-sm pull-right">More ... </a>
-                </section>
-            </div>
-        </div>
+                <div class="col-sm-6 col-md-4">
+                    <figure>
+                        <img class="content-image" src="{{ asset('frontend/assets/sinergi2.jpg') }}">
+                    </figure>
+                </div>
+                <div class="col-sm-6 col-md-8">
+                    <h4>Raymond Dragon</h4>
+                    <section style="font-size: 13px">
+                        <i class="icon-user"></i> Ichwan Setyawan
+                        <i class="icon-calendar"></i> 2021/12/21
+                    </section>
+                    <br>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt
+                        ut laoreet dolore magna aliquam erat volutpat.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea sequi aperiam consequatur earum consequuntur quos distinctio, molestias similique doloribus ad molestiae atque blanditiis reprehenderit exercitationem aliquid temporibus! Et impedit pariatur cumque ipsum unde in adipisci neque numquam voluptatem recusandae, atque aut exercitationem eaque laboriosam maiores possimus sit dolorem repudiandae rem?</p>
+                        
+                        <div>                            
+                            <a href="#" class="btn btn-primary btn-sm pull-right">More ... </a>
+                        </div>
+                        
+                    </div>
+                </div>
     </article>
+
     <article class=" p-4 shadow-sm my-3 bg-body rounded">
         <div class="row">
-            <div class="col-sm-6 col-md-4">
-                <figure>
-                    <img src="{{ asset('frontend/assets/sinergi2.jpg') }}">
-                </figure>
-            </div>
-            <div class="col-sm-6 col-md-8">
-                <h4>Raymond Dragon</h4>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt
-                    ut
-                    laoreet dolore magna aliquam erat volutpat.</p>
-
-                <section>
-                    <i class="icon-user"></i>Ichwan Setyawan
-                    <i class="icon-calendar"></i>1395/12/21
-                    <a href="#" class="btn btn-primary btn-sm pull-right">More ... </a>
-                </section>
-            </div>
-        </div>
+                <div class="col-sm-6 col-md-4">
+                    <figure>
+                        <img class="content-image" src="{{ asset('frontend/assets/sinergi2.jpg') }}">
+                    </figure>
+                </div>
+                <div class="col-sm-6 col-md-8">
+                    <h4>Raymond Dragon</h4>
+                    <section style="font-size: 13px">
+                        <i class="icon-user"></i> Ichwan Setyawan
+                        <i class="icon-calendar"></i> 2021/12/21
+                    </section>
+                    <br>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt
+                        ut laoreet dolore magna aliquam erat volutpat.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea sequi aperiam consequatur earum consequuntur quos distinctio, molestias similique doloribus ad molestiae atque blanditiis reprehenderit exercitationem aliquid temporibus! Et impedit pariatur cumque ipsum unde in adipisci neque numquam voluptatem recusandae, atque aut exercitationem eaque laboriosam maiores possimus sit dolorem repudiandae rem?</p>
+                        
+                        <div>                            
+                            <a href="#" class="btn btn-primary btn-sm pull-right">More ... </a>
+                        </div>
+                        
+                    </div>
+                </div>
     </article>
+
     <article class=" p-4 shadow-sm my-3 bg-body rounded">
         <div class="row">
-            <div class="col-sm-6 col-md-4">
-                <figure>
-                    <img src="{{ asset('frontend/assets/sinergi2.jpg') }}">
-                </figure>
-            </div>
-            <div class="col-sm-6 col-md-8">
-                <h4>Raymond Dragon</h4>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt
-                    ut
-                    laoreet dolore magna aliquam erat volutpat.</p>
+                <div class="col-sm-6 col-md-4">
+                    <figure>
+                        <img class="content-image" src="{{ asset('frontend/assets/sinergi2.jpg') }}">
+                    </figure>
+                </div>
+                <div class="col-sm-6 col-md-8">
+                    <h4>Raymond Dragon</h4>
+                    <section style="font-size: 13px">
+                        <i class="icon-user"></i> Ichwan Setyawan
+                        <i class="icon-calendar"></i> 2021/12/21
+                    </section>
+                    <br>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt
+                        ut laoreet dolore magna aliquam erat volutpat.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea sequi aperiam consequatur earum consequuntur quos distinctio, molestias m recusandae, atque aut exercitationem eaque laboriosam maiores possimus sit dolorem repudiandae rem?</p>
+                        
+                        <div>                            
+                            <a href="#" class="btn btn-primary btn-sm pull-right">More ... </a>
+                        </div>
+                        
+                    </div>
+                </div>
+    </article>
 
-                <section>
-                    <i class="icon-user"></i>Ichwan Setyawan
-                    <i class="icon-calendar"></i>2021/12/21
-                    <a href="#" class="btn btn-primary btn-sm pull-right">More ... </a>
-                </section>
-            </div>
-        </div>
+    <article class=" p-4 shadow-sm my-3 bg-body rounded">
+        <div class="row">
+                <div class="col-sm-6 col-md-4">
+                    <figure>
+                        <img class="content-image" src="{{ asset('frontend/assets/sinergi2.jpg') }}">
+                    </figure>
+                </div>
+                <div class="col-sm-6 col-md-8">
+                    <h4>Raymond Dragon</h4>
+                    <section style="font-size: 13px">
+                        <i class="icon-user"></i> Ichwan Setyawan
+                        <i class="icon-calendar"></i> 2021/12/21
+                    </section>
+                    <br>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt
+                        ut laoreet dolore magna aliquam erat volutpat.</p>
+                        <p>similique doloribus ad molestiae atque blanditiis reprehenderit exercitationem aliquid temporibus! Et impedit pariatur cumque ipsum unde in adipisci neque numquam voluptatem recusandae, atque aut exercitationem eaque laboriosam maiores possimus sit dolorem repudiandae rem?</p>
+                        
+                        <div>                            
+                            <a href="#" class="btn btn-primary btn-sm pull-right">More ... </a>
+                        </div>
+                        
+                    </div>
+                </div>
+    </article>
+
+    <article class=" p-4 shadow-sm my-3 bg-body rounded">
+        <div class="row">
+                <div class="col-sm-6 col-md-4">
+                    <figure>
+                        <img class="content-image" src="{{ asset('frontend/assets/sinergi2.jpg') }}">
+                    </figure>
+                </div>
+                <div class="col-sm-6 col-md-8">
+                    <h4>Raymond Dragon</h4>
+                    <section style="font-size: 13px">
+                        <i class="icon-user"></i> Ichwan Setyawan
+                        <i class="icon-calendar"></i> 2021/12/21
+                    </section>
+                    <br>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt
+                        ut laoreet dolore magna aliquam erat volutpat.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea sequi aperiam consequatur earum consequuntur quos distinctio, molestias similique doloribus ad molestiae atque blanditiis reprehenderit e eaque laboriosam maiores possimus sit dolorem repudiandae rem?</p>
+                        
+                        <div>                            
+                            <a href="#" class="btn btn-primary btn-sm pull-right">More ... </a>
+                        </div>
+                        
+                    </div>
+                </div>
     </article>
 @endsection
+            
--- resources/views/frontend/layouts/home.blade.php
+++ resources/views/frontend/layouts/home.blade.php
@@ -5,7 +5,7 @@
   <!-- Required meta tags -->
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="icon" type="image/x-icon" href="{{ asset('upload/desa/'.$desa->logo) }}"/>
+  {{-- <link rel="icon" type="image/x-icon" href="{{ asset('upload/desa/'.$desa->logo) }}"/> --}}
 
   <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
   <!-- Bootstrap CSS -->
